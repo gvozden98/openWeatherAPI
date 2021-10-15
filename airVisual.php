b1caf8ed5cecccc01a39d853b59bdd6adb51a4f7
gvozden98 <ognjen.gvozden98@gmail.com>
2021-10-15 02:55:33 +0200

    Working the pollution page, removed ip and key values

---
+++ airVisual.php
@@ -0,0 +1,136 @@
+<?php
+define("KEY", "");
+define("URL", "http://api.airvisual.com/v2/");
+
+$countries = array();
+requestCountries();
+function requestCountries()
+{
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://api.airvisual.com/v2/countries?key=' . KEY);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $resp = curl_exec($ch);
+    //get countries
+    if ($e = curl_error($ch)) {
+        echo $e;
+    } else {
+        $decoded = json_decode($resp, true);
+        getCountries($decoded);
+    }
+
+    curl_close($ch);
+}
+
+
+
+function getCountries($decoded)
+{
+    global $countries;
+    for ($i = 0; $i < 100; $i++) {
+        $countries[$i] = $decoded["data"][strval($i)]["country"];
+    }
+}
+
+
+
+
+//get user ip
+
+// if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+//     $ip = $_SERVER['HTTP_CLIENT_IP'];
+// } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+//     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+// } else {
+//     $ip = $_SERVER['REMOTE_ADDR'];
+// }
+
+// dummy ip because on localhost
+$ip = "";
+
+//get nearest city data
+$closeCity = array();
+function nearestCity($ip)
+{
+    global $closeCity;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://api.airvisual.com/v2/nearest_city?key=' . KEY);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "x-forwarded-for : $ip"
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $resp = curl_exec($ch);
+    //get countries
+    if ($e = curl_error($ch)) {
+        echo $e;
+    } else {
+        $closeCity = json_decode($resp, true);
+        //print_r($decoded);
+        print_r($closeCity["data"]["current"]["pollution"]);
+    }
+
+    curl_close($ch);
+}
+nearestCity($ip);
+
+?>
+
+
+<?php include "header.php" ?>
+<link rel="stylesheet" href="aqStyle.css">
+<div class="container">
+    <!-- <div class="jumbotron jumbotron-fluid">
+        <div class="container"> -->
+    <img src="aqi-index.jpg" alt="Air Pollution chart picture" id="pollutionChart">
+    <!-- </div>
+    </div> -->
+    <div class="card-deck">
+        <div class="card bg-light">
+            <div class="card-body text-center">
+                <div class="form-group">
+                    <label for="selectCity">Choose a Country</label>
+                    <select class="form-control" id="selectCity">
+                        <?php
+                        echo $countries[0];
+                        foreach ($countries as $country) {
+                            echo "<option>$country</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="card bg-light">
+            <!-- add a function for changing bg color on this card -->
+            <div class="card-body text-center d-flex p-2 container" id="pollutionData" style="background-color: #42f55d;">
+                <div class="container">
+                    <img src="ic-face-green.svg" alt="index picture" id="pollutionEmoji">
+                </div>
+                <div class="container">
+                    <?php
+                    $aqiIndex = $closeCity["data"]["current"]["pollution"]["aqius"];
+                    echo "<span style=\"font-size: 30px;\">$aqiIndex</span> Aqi";
+                    ?>
+
+                </div>
+                <div class="container">
+                    <?php
+                    $city = $closeCity["data"]["city"];
+                    $countryN = $closeCity["data"]["country"];
+                    echo "<h3>$city, $countryN</h3>";
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="card bg-danger">
+            <div class="card-body text-center">
+                <p class="card-text">Some text inside the fourth card</p>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="scrpitAirVisual.js"></script>
+<?php include "footer.php" ?>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Akronim&family=Londrina+Solid&display=swap" rel="stylesheet">
+    <title>WeatherNinja</title>
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php" id="logo">WeatherNinja</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="airVisual.php">Air Quality</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
